710a7f0914e7dfdb58a54a0f2e46af2b6ccd937d
myifengs <myifengs@gmail.com>
2021-06-25 13:56:55 +0800

    init project

---
+++ src/test/java/com/github/myifeng/commons/CommonsImportExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.myifeng.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsImportExportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
